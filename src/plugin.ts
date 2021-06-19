f02379d853166c2e0671bb0c948a9e85054b0925
Oscar Beaumont <oscar@otbeaumont.me>
2021-06-20 05:40:10 +0800

    fix: :art: cleaned up the code + bumped version

--- src/plugin.ts
+++ src/plugin.ts
@@ -1,33 +1,30 @@
 import type { Plugin } from 'vite'
 import { transformSync } from '@babel/core'
 
-type PluginConfig = {}
-
-export default (config: PluginConfig = {}): Plugin => {
-  return {
-    name: 'vite:relay',
-    transform(src: any, id: any) {
-      if (/.tsx/.test(id) && src.includes('graphql`')) {
-        const out = transformSync(src, {
-          plugins: [
-            [
-              'relay',
-              {
-                eagerESModules: true,
-              },
-            ],
+export default {
+  name: 'vite:relay',
+  transform(src, id) {
+    let code = src
+    if (/.(t|j)sx?/.test(id) && src.includes('graphql`')) {
+      const out = transformSync(src, {
+        plugins: [
+          [
+            'relay',
+            {
+              eagerESModules: true,
+            },
           ],
-          code: true,
-        })
+        ],
+        code: true,
+      })
 
-        if (out === null) throw new Error('vite-plugin-react Failed to build')
-        src = out.code
-      }
+      if (!out?.code) throw new Error('vite-plugin-react Failed to build')
+      code = out.code
+    }
 
-      return {
-        code: src,
-        map: null,
-      }
-    },
-  }
-}
+    return {
+      code,
+      map: null,
+    }
+  },
+} as Plugin
